594a3f5446462ac55c6675eafd7f16b5a348008e
leftidev <jes.bergstrom@gmail.com>
2024-09-27 10:29:19 +0300

    Introduced vulnerability #4, Security logging and Monitoring failures.

--- pollingapp/apps.py
+++ pollingapp/apps.py
@@ -5,5 +5,5 @@ class PollingappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pollingapp'
     
-    def ready(self):
-        import pollingapp.signals
+    #def ready(self):
+    #    import pollingapp.signals
